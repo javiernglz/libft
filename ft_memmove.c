b81b163cc0bc268030cc461a58ec9f66ee4e6aac
Francisco Javier Navarrete Gonzalez <frnavarr@c2r4s3.42malaga.com>
2024-09-13 14:52:43 +0200

    memmove v1

---
+++ ft_memmove.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 14:45:15 by frnavarr          #+#    #+#             */
+/*   Updated: 2024/09/13 14:52:21 by frnavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dest > src)
+	{
+		while (n > 0)
+		{
+			((char *)dest)[n - 1] = ((char *)src)[n - 1];
+			n--;
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			((char *)dest)[i] = ((char *)src)[i];
+			i++;
+		}
+	}
+	return (dest);
+}
+
+int	main(void)
+{
+	char	src[] = "Malaguita";
+	char	dest[20];
+	size_t	n;
+	
+	n = 13;
+
+	ft_memmove(dest, src, n);
+	printf("%s\n", dest);
+	return (0);
+}
