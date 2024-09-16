86a2a099ffd8d8ee17754e289002f02cbca76429
Francisco Javier Navarrete Gonzalez <frnavarr@c1r2s3.42malaga.com>
2024-09-16 16:12:39 +0200

    calloc usando bzero

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 15:59:20 by frnavarr          #+#    #+#             */
+/*   Updated: 2024/09/16 16:05:49 by frnavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
