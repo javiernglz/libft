9e2fc4c24daf1acc93f17426de3dbda472466dab
Francisco Javier Navarrete Gonzalez <frnavarr@c3r8s2.42malaga.com>
2024-09-26 19:52:42 +0200

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 18:42:24 by frnavarr          #+#    #+#             */
+/*   Updated: 2024/09/26 19:02:33 by frnavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t			len;
+	unsigned int	i;
+	char			*res;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	res = malloc((len + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (i < len)
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[len] = '\\0';
+	return (res);
+}
