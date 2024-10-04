d79eb2871c806f74fe1080f2f60cde3f7e9b6027
Francisco Javier Navarrete Gonzalez <frnavarr@c2r7s1.42malaga.com>
2024-10-04 13:15:33 +0200

    itoa fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,24 +6,62 @@
 /*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 12:46:29 by frnavarr          #+#    #+#             */
-/*   Updated: 2024/10/02 13:04:23 by frnavarr         ###   ########.fr       */
+/*   Updated: 2024/10/04 13:07:35 by frnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_nbrlen(long int nb)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (nb == 0)
+		return (1);
+	if (nb < 0)
+		i++;
+	while (nb != 0)
+	{
+		nb = nb / 10;
+		i++;
+	}
+	return (i);
+}
+
 char	*ft_itoa(int n)
 {
-	char	*res;
-	int		len;
-	int		is_negative;
+	char		*res;
+	int			nlen;
+	long int	nbr;
 
-	res = (char *)malloc((len + 1) * sizeof(char));
+	nbr = n;
+	nlen = ft_nbrlen(nbr);
+	res = (char *)malloc((nlen + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
-	if (is_negative)
-		n = -n;
-	while (n > 0)
-		res = (n % 10) + 48;
+	res[nlen--] = '\0';
+	if (nbr == 0)
+		res[0] = '0';
+	if (nbr < 0)
+	{
+		res[0] = '-';
+		nbr = -nbr;
+	}
+	while (nbr > 0)
+	{
+		res[nlen--] = (nbr % 10) + 48;
+		nbr = nbr / 10;
+	}
 	return (res);
 }
+
+/* int	main(void)
+{
+	char	*res;
+
+	res = ft_itoa(-2540);
+	printf("The itoa number is: %s\n", res);
+	return (0);
+}
+ */
--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,18 @@
 /*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 17:15:35 by frnavarr          #+#    #+#             */
-/*   Updated: 2024/10/01 16:12:31 by frnavarr         ###   ########.fr       */
+/*   Updated: 2024/10/04 13:14:16 by frnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* #include "libft.h"
+#include "libft.h"
 
-int	word_count (char const *s, char c)
+static int	ft_countstr (char const *s, char c)
 {
-	int	count;
-	int	i;
-	char *str;
-	
+	int		count;
+	int		i;
+	char 	*str;
+
 	str = ft_strtrim(s, &c);
 	i = 0;
 	count = 0;
@@ -33,16 +33,25 @@ int	word_count (char const *s, char c)
 		}
 	}
 	return (count + 1);
-			
+
+}
+static int	ft_subslen()
+{
+
+}
+
+static int ft_strfree ()
+{
+
 }
 
 char **ft_split(char const *s, char c)
 {
 	char **split;
 	size_t	res;
-	res = word_count(s, c);
+	res = ft_countstr(s, c);
 	
-	split = (char **)malloc( res * sizeof(*char));
+	split = (char **)malloc( res * sizeof(char **));
 }
 
 int main (void)
@@ -52,4 +61,4 @@ int main (void)
 	res = word_count("  Hola   cuantas palabras    hay", ' ');
 	printf("%d", res);
 	return (0);
-} */
+}
